da434e4ae9314e5a9cea59e484d935606997a52d
obikakelvin <obikakelvin.2255@gmail.com>
2022-11-02 12:05:35 -0700

    Code refactor and restructure

--- src/js/home.js
+++ src/js/home.js
@@ -1,21 +1,13 @@
-
 import { getCategories } from '/js/index.js'
 
-// // show reports by filters
+// show reports by filters
 let reportArrays=await getCategories('categories','name','park');
-for(let report of reportArrays.values()){ 
-    console.log('category: '+report.category) 
-    console.log('Title: '+ report.title)
-    console.log('Address: '+ report.address)
-    console.log('Created At: '+new Date(report.createdAt.seconds*1000).toLocaleString())
-
+for(let report of reportArrays.values()){  
 }
 
 
 // show all reports
-// let reportMap=await getCategories('categories');
-// for(let report of reportMap.values()){
-//     console.log('Title:'+ report.title)
-//     console.log('Address:'+ report.address)
-//     // console.log('Created At'+new Date(report.createdAt.seconds*1000).toLocaleString())
-// }
+let reportMap=await getCategories('categories');
+for(let report of reportMap.values()){
+    console.log('address: '+report.address)
+}
