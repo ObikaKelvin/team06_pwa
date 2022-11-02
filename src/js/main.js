9d8eb1a4c6963cc2d511da55b976b6f503713234
obikakelvin <obikakelvin.2255@gmail.com>
2022-11-02 11:59:28 -0700

    Code refactor and restructure

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker
+        .register("/serviceWorker.js")
+    })
+}
+
+console.log("CONNECTED")
+
