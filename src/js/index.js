2823fa02ba7a49f36d5c5b33e4474f29657f86ad
mahimamukhi <mukhi.mahima7@gmail.com>
2022-11-15 17:42:34 -0800

    Merge branch 'master' of https://github.com/ObikaKelvin/team06_pwa into mahima

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
-import './services/worker';
 
+import './services/worker';
 import AccountController from './controllers/AccountController';
+import ReportController from './controllers/ReportController';
 
 const account = new AccountController();
 
 account.init();
+
+
+const accountController = new AccountController();
+const reportController = new ReportController();
+
+accountController.init();
+reportController.init();
