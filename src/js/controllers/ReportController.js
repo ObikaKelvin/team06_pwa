a44aa96064f3e5bed004c05073496473f0b31223
sdsodha <ssdsodha@gmail.com>
2022-11-22 13:42:08 -0800

    Create Report

--- src/js/controllers/ReportController.js
+++ src/js/controllers/ReportController.js
@@ -1,18 +1,41 @@
 import location from "../services/location";
 import { app, db } from '../services/firebase';
-import { doc, getDocs, addDoc, collection, query } from "firebase/firestore";
+import { doc, getDocs, addDoc, collection, query, where } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, uploadString } from "firebase/storage";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import Report from "../models/Report";
+
+const options = {
+    searchOptions: {
+        key: 'LFLu78rwy2GqhdhXDAZqkEDwa37wbytt',
+        language: 'en-GB',
+        limit: 5
+    },
+    autocompleteOptions: {
+        key: 'LFLu78rwy2GqhdhXDAZqkEDwa37wbytt',
+        language: 'en-GB'
+    }
+};
+let ttSearchBox = null;
+let searchBoxHTML = null;
+if(document.getElementById('address')) {
+    console.log(document.getElementById('address'))
+    ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
+    searchBoxHTML = ttSearchBox.getSearchBoxHTML();
+}
 
 class ReportController {
 
     images = [];
 
     init = () => {
+        
+        let addressContainer = document.getElementById('address')
+        if(addressContainer) addressContainer.append(searchBoxHTML);
         this.setupEventListener();
         this.getCategories();
+        this.getMyReports();
     }
 
     getCategories = async () => {
@@ -28,9 +51,9 @@ class ReportController {
     }
 
     setupEventListener = () => {
-        document.querySelector("#camera--upload").addEventListener("click", this.cameraStart);
-        document.querySelector("#camera--trigger").addEventListener("click", this.caputreImg);
-        reportSubmit.addEventListener('click', this.createReport);
+        document.querySelector("#camera--upload")?.addEventListener("click", this.cameraStart);
+        document.querySelector("#camera--trigger")?.addEventListener("click", this.caputreImg);
+        document.querySelector("#reportSubmit")?.addEventListener('click', this.createReport);
     }
 
 
@@ -70,12 +93,16 @@ class ReportController {
         const category = document.querySelector("#category").value;
         const categoryRef = doc(db, 'categories', category);
         const description = document.querySelector("#description").value;
-        const address = document.querySelector("#address").value;
+        const address = ttSearchBox.getValue();
+
+        console.log('address')
+        console.log(address)
         const locationDetails = document.querySelector("#locationDetails").value;
 
         
         const auth = getAuth();
         let uid = null
+        
         onAuthStateChanged(auth, async (user) => {
             if (user) {
                 // User is signed in, see docs for a list of available properties
@@ -91,9 +118,9 @@ class ReportController {
                 await Promise.all(this.images.map( async (image) => {
                     // Create a root reference
                     const storage = getStorage(app, "gs://the-citizen-d6e2b.appspot.com/");
-
+                    const imageName = `report-${uid}-${Date.now()}`
                     // Create a reference to 'mountains.jpg'
-                    const mountainsRef = ref(storage, "images11");
+                    const mountainsRef = ref(storage, imageName);
 
                     // 'file' comes from the Blob or File API
                     await uploadString(mountainsRef, image, 'data_url');
@@ -109,6 +136,64 @@ class ReportController {
         });
 
         
+    }
+
+    getMyReports = async () => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const userRef = doc(db, 'users', user.uid);
+
+                const reportsRef = collection(db, 'reports')
+                const reports = await getDocs(query(reportsRef, where("postedBy", "==", userRef)));
+
+                reports.forEach((reportData) => {
+                    const report = reportData.data();
+                    console.log(report)
+                    const reportRow = document.createElement("tr");
+                    
+                    const reportTitle = document.createElement("td");
+                    const reportCreated = document.createElement("td");
+                    const reportUpdated = document.createElement("td");
+                    const reportStatus = document.createElement("td");
+                    const reportStatusParagh = document.createElement("p");
+
+                    const createdDate = new Date(report.createdAt?.toDate());
+                    const createdDateFormat = `
+                        ${new Intl.DateTimeFormat('en', { month: "short"}).format(createdDate)} 
+                        ${createdDate.getDay()}, ${createdDate.getFullYear()} | 
+                        ${createdDate.getHours()}:${createdDate.getMinutes()}am
+                    `;
+
+                    const updatedDate = new Date(report.updatedAt?.toDate());
+                    const updatedDateFormat = `
+                        ${new Intl.DateTimeFormat('en', { month: "short"}).format(updatedDate)} 
+                        ${updatedDate.getDay()}, ${updatedDate.getFullYear()} | 
+                        ${updatedDate.getHours()}:${updatedDate.getMinutes()}am
+                    `;
+
+                    reportTitle.textContent = report.title;
+                    reportCreated.textContent = createdDateFormat;
+                    reportUpdated.textContent = updatedDateFormat;
+                    reportStatusParagh.textContent = report.status;
+                    reportStatusParagh.classList.add(`table__status`, `table__status--review`)
+                    reportStatus.append(reportStatusParagh)
+                    
+                    reportRow.append(...[
+                        reportTitle,
+                        reportCreated,
+                        reportUpdated,
+                        reportStatus
+                    ])
+
+                    document.getElementById("report__body").appendChild(reportRow);
+
+
+                });
+            } 
+        });
     }
 
 }
