913b5513f2c187f95eb703671b04796f2c7f82d2
sdsodha <ssdsodha@gmail.com>
2022-11-23 09:59:03 -0800

    Remove Local storage key of Report
    This fix clears the Local Storage after displaying data to the Report Detail page

--- src/js/reportdetail.js
+++ src/js/reportdetail.js
@@ -96,6 +96,8 @@ console.log("Map Called");
        
        
 console.log(`Map Data ${center}`);
+localStorage.removeItem('Report');
+
 
 /*let mapView=document.getElementById('mapView')
 let searchDiv=document.getElementById('search1')
