405c17a9768b2954c4dcc04b027086c989d1b626
obikakelvin <obikakelvin.2255@gmail.com>
2022-11-30 09:22:38 -0800

    fixed merge issues - working update

--- src/js/nearest.js
+++ src/js/nearest.js
@@ -5,8 +5,10 @@ nearest.addEventListener('click', () => {
   getUserLocation();
 })
 
+
+
 //get user current location
-function getUserLocation() {
+export function getUserLocation() {
   try {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -34,34 +36,39 @@ function getUserLocation() {
 async function getReports(position) {
   let categories = await getCategories('categories')
   const container = document.getElementById('cards')
+  let welcomeHeader = document.getElementById('nearestCount')
+  
   let output = '';
+  let count=0;
   for (let report of categories.values()) {
- console.log('lat '+report.latitude.toFixed(1).toString())
- console.log('lat '+report.longitude.toFixed(1).toString())
-
+    console.log('lat '+report.latitude.toFixed(1).toString())
+    console.log('lat '+report.longitude.toFixed(1).toString())
+    
     if (position.coords.latitude.toFixed(1).toString()===report.latitude.toFixed(1).toString() &&
       position.coords.longitude.toFixed(1).toString() === report.longitude.toFixed(1).toString()) {
+        count++;
         let image=report.images.toLowerCase();
         //  let imageUrl=`./assets/images/${report.category}.jpg`;
-         let imageUrl=`./assets/images/${image}`;
-      output += `<div class="card">
-    <img class="cardImg" src=${imageUrl} alt="img1">
-    <div class="cardDetail">
+        let imageUrl=`./assets/images/${image}`;
+        output += `<div class="card">
+        <img class="cardImg" src=${imageUrl} alt="img1">
+        <div class="cardDetail">
         <h3>${report.title}</h3>
         <span class="location"><h4>${report.address}</h4></span>
         <span class="time"><h4>${new Date(report.createdAt.seconds * 1000).toLocaleString()}</h4></span>
         <div class="tags">
-            <span class="filterIcon activeIcon clear"><a href="">${report.category}</a></span>                
+        <span class="filterIcon activeIcon clear"><a href="">${report.category}</a></span>                
         </div>
-    </div>
-</div>`;
-    }
+        </div>
+        </div>`;
+        welcomeHeader.innerHTML=`<h3>There's <a href="#" >${count} new issues</a> reported in your neighbourhood</h3>`
+      }
   }
   // show output
   container.innerHTML = output;
 }
 
-
+// export default getUserLocation;
 
 
 
